ef138fef9d78099457ad04486e7966547590fedb
Konsta <konsta09@gmail.com>
2012-05-25 16:45:35 +0300

    remove live wallpapers
    Change-Id: I56cbd4adb9f15b4feb7bae0a49915d4469e06951

--- device.mk
+++ device.mk
@@ -61,10 +61,11 @@ PRODUCT_PACKAGES += \
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-        LiveWallpapers \
         LiveWallpapersPicker \
-        VisualizationWallpapers \
         librs_jni
+
+#        LiveWallpapers \
+#        VisualizationWallpapers \
 
 # Other
 PRODUCT_PACKAGES += \
