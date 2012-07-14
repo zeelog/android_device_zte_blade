54cef36395df6996cbe942f0ae8a5cfa3e086151
Konsta <konsta09@gmail.com>
2012-07-14 12:20:17 +0300

    live wallpaper support
    Change-Id: I71d4f8fd9424cf87666c38e230099af0a374db0b

--- device.mk
+++ device.mk
@@ -64,8 +64,8 @@ PRODUCT_PACKAGES += \
         LiveWallpapersPicker \
         librs_jni
 
-#        LiveWallpapers \
-#        VisualizationWallpapers \
+PRODUCT_COPY_FILES := \
+        packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Other
 PRODUCT_PACKAGES += \
