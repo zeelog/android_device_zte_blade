7db86910f683b809fcce66e8557a3c80c5246b47
Konsta <konsta09@gmail.com>
2012-07-20 23:49:25 +0300

    update market fingerprint, N880E jelly bean
    Change-Id: Ie12072ab4917b756e268aecc4f866795734891be

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=blade BUILD_ID=IMM76D BUILD_FINGERPRINT=ZTE/P772A10D/atlas40:4.0.4/IMM76D/20120608.113547:user/release-keys PRIVATE_BUILD_DESC="P772A10D-user 4.0.4 IMM76D 20120608.113547 release-keys" BUILD_NUMBER=20120608.113547
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=blade BUILD_ID=JRO03C BUILD_FINGERPRINT=ZTE/N880E_JB/atlas40:4.1.1/JRO03C/eng.songsy.20120718.233441:eng/test-keys PRIVATE_BUILD_DESC="N880E_JB-eng 4.1.1 JRO03C eng.songsy.20120718.233441 test-keys" BUILD_NUMBER=eng.songsy.20120718.233441
 
 PRODUCT_NAME := cm_blade
 PRODUCT_DEVICE := blade
