60dc3939773fbeff0a7dce4637f920c6b0c4b8da
Konsta <konsta09@gmail.com>
2013-01-02 15:52:41 +0200

    update recovery kernel & recovery init
    Change-Id: I4b3ce67b1dd5712db8913771cc0351e05bc58e50

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,14 +37,13 @@ TARGET_SPECIFIC_HEADER_PATH := device/zte/blade/include
 # Recovery
 BOARD_CUSTOM_GRAPHICS := ../../../device/zte/blade/recovery/graphics.c
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/zte/blade/recovery/recovery_ui.c
+TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/blade/recovery/recovery_kernel
 TARGET_RECOVERY_INITRC := device/zte/blade/recovery/recovery.rc
 TARGET_RECOVERY_FSTAB := device/zte/blade/recovery/recovery.fstab
-BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/zte/zte-kernel-msm7x27
 TARGET_KERNEL_CONFIG := cyanogen_blade_defconfig
-TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/blade/recovery/recovery_kernel
 BOARD_KERNEL_BASE := 0x02600000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=blade console=null
 
