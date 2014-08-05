KonstaT/android_device_zte_atlas40

use common proprietary extraction scripts
Change-Id: I8c1e68d9cad64730c77a992ecff43520ab2d9ee1

extract-files.sh
@@ -1,40 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+export VENDOR=zte
+export DEVICE=atlas40
 
-VENDOR=zte
-DEVICE=atlas40
-
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g'`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    if [ "$SRC" = "adb" ]; then
-      adb pull /system/$FILE $BASE/$FILE
-    else
-      cp $SRC/system/$FILE $BASE/$FILE
-    fi
-
-done
-
-./setup-makefiles.sh
+../../../vendor/cm/tools/extract-files.sh $@

setup-makefiles.sh
@@ -1,85 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-VENDOR=zte
-DEVICE=atlas40
-OUTDIR=vendor/$VENDOR/$DEVICE
-MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
+export VENDOR=zte
+export DEVICE=atlas40
 
-(cat << EOF) > $MAKEFILE
-# Copyright (C) 2011 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
-# Prebuilt libraries that are needed to build open-source libraries
-PRODUCT_COPY_FILES += \\
-    vendor/zte/atlas40/proprietary/lib/liboncrpc.so:obj/lib/liboncrpc.so \\
-    vendor/zte/atlas40/proprietary/lib/libnv.so:obj/lib/libnv.so
-
-PRODUCT_COPY_FILES += \\
-EOF
-
-LINEEND=" \\"
-COUNT=`cat proprietary-files.txt | grep -v ^# | grep -v ^$ | wc -l | awk {'print $1'}`
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-    COUNT=`expr $COUNT - 1`
-    if [ $COUNT = "0" ]; then
-        LINEEND=""
-    fi
-    echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
-
-done
-
-(cat << EOF) > ../../../$OUTDIR/$DEVICE-vendor.mk
-# Copyright (C) 2011 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
-# Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/$VENDOR/$DEVICE/overlay
-
-\$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
-EOF
-
-(cat << EOF) > ../../../$OUTDIR/BoardConfigVendor.mk
-# Copyright (C) 2011 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
-USE_CAMERA_STUB := false
-EOF
+../../../vendor/cm/tools/setup-makefiles.sh

zeelog/android_device_zte_blade

Conflicts:
	device-proprietary-files.txt
	extract-files.sh
	setup-makefiles.sh

extract-files.sh
@@ -1,35 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-## usage: extract-files.sh $1 $2
-## $1 and $2 are optional
-## if $1 = unzip the files will be extracted from zip file (if $1 = anything else 'adb pull' will be used
-## $2 specifies the zip file to extract from (default = ../../../${DEVICE}_update.zip)
+export VENDOR=zte
+export DEVICE=blade
 
-VENDOR=zte
-DEVICE=blade
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-if [ -z "$2" ]; then
-    ZIPFILE=../../../${DEVICE}_update.zip
-else
-    ZIPFILE=$2
-fi
-
-if [ "$1" = "unzip" -a ! -e $ZIPFILE ]; then
-    echo $ZIPFILE does not exist.
-else
-    for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-        DIR=`dirname $FILE`
-	if [ ! -d $BASE/$DIR ]; then
-            mkdir -p $BASE/$DIR
-	fi
-	if [ "$1" = "unzip" ]; then
-            unzip -j -o $ZIPFILE system/$FILE -d $BASE/$DIR
-	else
-            adb pull /system/$FILE $BASE/$FILE
-	fi
-    done
-fi
-./setup-makefiles.sh
+../../../vendor/cm/extract-files.sh $@

setup-makefiles.sh
@@ -1,83 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-VENDOR=zte
-DEVICE=blade
-OUTDIR=vendor/$VENDOR/$DEVICE
-MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
+export VENDOR=zte
+export DEVICE=blade
 
-(cat << EOF) > $MAKEFILE
-# Copyright (C) 2011 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
-PRODUCT_COPY_FILES := \\
-    $OUTDIR/proprietary/lib/libloc.so:obj/lib/libloc.so
-
-PRODUCT_COPY_FILES += \\
-EOF
-
-LINEEND=" \\"
-COUNT=`cat proprietary-files.txt | grep -v ^# | grep -v ^$ | wc -l | awk {'print $1'}`
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-    COUNT=`expr $COUNT - 1`
-    if [ $COUNT = "0" ]; then
-        LINEEND=""
-    fi
-    echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
-
-done
-
-(cat << EOF) > ../../../$OUTDIR/$DEVICE-vendor.mk
-# Copyright (C) 2011 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
-# Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/$VENDOR/$DEVICE/overlay
-
-\$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
-EOF
-
-(cat << EOF) > ../../../$OUTDIR/BoardConfigVendor.mk
-# Copyright (C) 2011 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-
-USE_CAMERA_STUB := false
-EOF
+../../../vendor/cm/tools/setup-makefiles.sh